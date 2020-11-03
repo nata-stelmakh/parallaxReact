a5d847fbd4d3b66636dbba75e664f4f3dd1c0275
Author Name <stelmakh.natallia@gmail.com>
2020-11-02 22:39:21 -0500

    starting react parallax app

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <TodoProvider>
+    <App />, document.getElementById("root")
+  </TodoProvider>
+);
